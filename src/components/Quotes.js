e440eb6f8dc7963fb20b57f7e0681f9660bc353c
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-09-14 11:20:24 -0400

    implement useEffect to setQuotes

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,11 +1,20 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+
+const url = 'http://localhost:3001'
 
 const Quotes = () => {
 
     const [quotes, setQuotes] = useState([])
 
+    useEffect(() => {
+        fetch(url + '/quotes')
+        .then(resp => resp.json())
+        .then(data => setQuotes(data))
+    }, [setQuotes])
+
     return(
         <div>
+            {console.log(quotes)}
             The Quotes Go Here!
         </div>
     )
