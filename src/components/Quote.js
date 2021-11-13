fb9b4b86436ab62ac54d3c321dd546296182735e
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-11-12 19:24:26 -0500

    begin fixing quotation props passing

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const Quote = ({quoteData: { body, book_num, section_num }}) => {
+const Quote = ({quoteData/*: { body, book_num, section_num }*/}) => {
     return(
         <div>
-            <small>Book {book_num} Section {section_num}</small>
-            <p>"{body}"</p>
+            {console.log(quoteData)}
+            {/* <small>Book {book_num} Section {section_num}</small> */}
+            {/* <p>"{body}"</p> */}
         </div>
     )
 }
