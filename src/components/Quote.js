f37813eae099dc7ad74b5bd51e36e88023c49cce
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-11-12 20:24:37 -0500

    update quote fetching and rendering

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Quote = ({quoteData/*: { body, book_num, section_num }*/}) => {
+const Quote = ({ quoteData: { body, book_num, section_num, translator_name } }) => {
     return(
         <div>
-            {console.log(quoteData)}
-            {/* <small>Book {book_num} Section {section_num}</small> */}
-            {/* <p>"{body}"</p> */}
+            <small>Book {book_num} Section {section_num}</small>
+            <p>"{body}"</p>
+            <p>(trans. {translator_name})</p>
         </div>
     )
 }
